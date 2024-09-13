4ff59b610a84a9f0817b927f84388dc9f0ff8c3f
devinfafara <devinfafara@gmail.com>
2024-09-13 17:23:04 +0200

    Deploy to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/'
+    : '/',
   chainWebpack: config => {
     config.module
       .rule('pdf')
